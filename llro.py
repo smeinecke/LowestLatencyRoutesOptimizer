7fa5d91e2742185af240fdc48a36667b16529669
Stefan M <meinecke@comeso.org>
2024-05-30 14:40:30 +0200

    Fixes + add fallback routes config
    - fix clear_routes
    - make /usr/sbin/ip binary path configurable
    - remove iproute2 dependency
    - handle error response from "ip" binary
    - remove routing if all hosts offline
    - add fallback routes

--- llro.py
+++ llro.py
@@ -4,8 +4,7 @@ import logging
 from icmplib import async_multiping
 import asyncio
 import argparse
-# import pyroute2
-import os
+import subprocess
 
 logging.basicConfig(
     level=logging.INFO,
@@ -45,41 +44,60 @@ class LowestLatencyRoutesOptimizer:
         Returns:
             None
         """
-        source_ips = set()
-        for interface, src_ips in self.config['interfaces'].items():
-            source_ips.update(src_ips)
+        hosts = set()
+        for host in self.config['monitor']:
+            hosts.add(host)
+            if host in self.config.get('also_route', {}):
+                hosts.update(self.config['also_route'][host])
 
-        dst_ips = set(self.config['monitor'])
-        for _dst_ips in self.config.get('also_route', {}).values():
-            dst_ips.update(_dst_ips)
-
-        # as pyroute2 is unstabile as hell i simply replace it with local "ip route" calls.
-        for host in source_ips:
+        for host in hosts:
+            cmd = f"{self.config.get('ip_bin', '/usr/sbin/ip')} route del {host}/32"
+            logging.debug("cmd: %s", cmd)
             try:
-                os.system(f"ip route del {host}/32")
-            except Exception as e:
-                logging.exception(e)
-
-        """
-        with pyroute2.NDB() as ndb:
-            for host in dst_ips:
-                try:
-                    with ndb.routes[f"{host}/32"] as route:
-                        gw = route['gateway']
-                        if gw not in source_ips:
-                            continue
-
-                        logging.info("Remove %s => %s", host, gw)
-                        route.remove()
-                except KeyError:
+                logging.debug(subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True))
+            except subprocess.CalledProcessError as e:
+                if 'RTNETLINK answers: No such process' in e.output.decode('utf-8'):
                     continue
+                logging.error("command '%s' return with error (code %s): %s", e.cmd, e.returncode, e.output)
+            except Exception as e:
+                logging.error(e)
+
+        self.current_routes = {}
+
+        # set fallback routes as no route set
+        for host, gateway in self.config.get('fallback_routes', {}).items():
+            self.apply_route_config(host, gateway)
+
+    def clear_route(self, host):
         """
+        Removes the route for the given host.
+
+        Parameters:
+            host (str): The host to remove the route for.
+
+        Returns:
+            None
+        """
+
+        logging.info("Remove %s", host)
+        cmd = f"{self.config.get('ip_bin', '/usr/sbin/ip')} route del {host}/32"
+        logging.debug("cmd: %s", cmd)
+        try:
+            logging.debug(subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True))
+        except subprocess.CalledProcessError as e:
+            if 'RTNETLINK answers: No such process' in e.output.decode('utf-8'):
+                return
+            logging.error("command '%s' return with error (code %s): %s", e.cmd, e.returncode, e.output)
+        except Exception as e:
+            logging.error(e)
 
     def apply_route_config(self, host, gateway):
         """
         Applies the route configuration.
 
-        Adds the route to the routing table for the given host and gateway. Additionally, if the also_route configuration is specified, it will also add the route for the given host to the routing table of the specified hosts.
+        Adds the route to the routing table for the given host and gateway.
+        Additionally, if the also_route configuration is specified, it will also add the route for
+        the given host to the routing table of the specified hosts.
 
         Parameters:
             host (str): The host to add the route for.
@@ -91,28 +109,30 @@ class LowestLatencyRoutesOptimizer:
         hosts_to_add = [host] + self.config.get('also_route', {}).get(host, [])
 
         logging.info("Apply %s => %s", host, gateway)
-        try:
-            for _host in hosts_to_add:
-                # as pyroute2 is unstabile as hell i simply replace it with local "ip route" calls.
-                if host not in self.current_routes:
-                    os.system(f"ip route add {_host}/32 via {gateway}")
-                os.system(f"ip route replace {_host}/32 via {gateway}")
-                """
-                with pyroute2.NDB() as ndb:
-                    try:
-                        with ndb.routes[{'dst': f"{host}/32"}] as route:
-                            logging.info("Update %s => %s (%s)", host, gateway, route['gateway'])
-                            route.set('gateway', gateway)
-                    except pyroute2.netlink.exceptions.NetlinkError:
-                        pass
-                    except KeyError:
-                        logging.info("Add %s => %s", host, gateway)
-                        ndb.routes.create(dst=f"{host}/32", gateway=gateway).commit()
-                """
-            self.current_routes[host] = gateway
-        except Exception as e:
-            logging.exception(e)
+        for _host in hosts_to_add:
+            if _host not in self.current_routes:
+                cmd = f"{self.config.get('ip_bin', '/usr/sbin/ip')} route add {_host}/32 via {gateway}"
+                logging.debug("cmd: %s", cmd)
+                try:
+                    logging.debug(subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True))
+                    continue
+                except subprocess.CalledProcessError as e:
+                    if 'RTNETLINK answers: File exists' not in e.output.decode('utf-8'):
+                        logging.error("command '%s' return with error (code %s): %s", e.cmd, e.returncode, e.output)
+                        continue
+                except Exception as e:
+                    logging.exception(e)
 
+            cmd = f"{self.config.get('ip_bin', '/usr/sbin/ip')} route replace {_host}/32 via {gateway}"
+            logging.debug("cmd: %s", cmd)
+            try:
+                logging.debug(subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True))
+            except subprocess.CalledProcessError as e:
+                logging.error("command '%s' return with error (code %s): %s", e.cmd, e.returncode, e.output)
+            except Exception as e:
+                logging.exception(e)
+
+            self.current_routes[_host] = gateway
 
     async def run_async(self):
         """
@@ -130,6 +150,8 @@ class LowestLatencyRoutesOptimizer:
         sums = {}
         loop = asyncio.get_event_loop()
         while True:
+
+            # send ICMP requests
             tasks = []
             sources = []
             for etherif, src_ips in self.config['interfaces'].items():
@@ -138,8 +160,10 @@ class LowestLatencyRoutesOptimizer:
                         'test_count', 3), source=src_ip, interval=float(self.config.get('test_interval', 0.5)))))
                     sources.append(src_ip)
 
+            # wait and aggregate results
             result = await asyncio.gather(*tasks)
 
+            # process results
             host_data = {}
             sources_up = set()
             for x, hosts in enumerate(result):
@@ -167,6 +191,7 @@ class LowestLatencyRoutesOptimizer:
 
                     host_data[host.address].append((source, host.avg_rtt, host.packet_loss))
 
+            # sort by newest
             force_reset = False
             for host, results in host_data.items():
                 if host in self.current_routes and self.current_routes[host] not in sources_up:
@@ -174,7 +199,9 @@ class LowestLatencyRoutesOptimizer:
                 host_data[host] = sorted(results, key=lambda y: (y[2], y[1]))
                 logging.debug("%s: %s", host, host_data[host])
 
+            # apply routes
             checks += 1
+            valid_source_found = []
             if checks >= self.config.get('test_count', 10) or force_reset or not self.current_routes:
                 for host, results in sums.items():
                     host_data = []
@@ -186,29 +213,49 @@ class LowestLatencyRoutesOptimizer:
 
                     host_data = sorted(host_data, key=lambda y: (y[2], y[1]))[0]
 
-                    logging.debug(self.current_routes)
-                    if host not in self.current_routes:
-                        # no routing set
+                    # no routing set
+                    if host not in self.current_routes or self.current_routes[host] not in results:
+                        valid_source_found.append(host)
                         self.apply_route_config(host, host_data[0])
                         continue
 
+                    # no change
                     if self.current_routes[host] == host_data[0]:
-                        logging.debug("Current route is fastest route")
+                        valid_source_found.append(host)
+                        logging.debug("%s: Current route is already the fastest route", host)
                         continue
 
+                    # paketloss
                     if results[self.current_routes[host]]['loss'] > self.config.get('paketloss_threshold', 5):
-                        logging.warning("Current route has paketloss, need to switch")
+                        logging.warning("%s: Current route has paketloss, need to switch", host)
                     else:
+                        # check rtt difference between current and fastest route
                         rtt_diff = results[self.current_routes[host]]['rtt'] - host_data[1]
+                        logging.debug("%s: rtt_diff: %s, (%s) %s (%s) %s ", host, rtt_diff, self.current_routes[host], results[self.current_routes[host]]['rtt'], host_data[0], host_data[1])
 
                         if rtt_diff < self.config.get('rtt_threshold', 20):
-                            logging.info("Route not changed to %s, rtt difference %s < threshold %s", host_data[0], rtt_diff, self.config.get('rtt_threshold', 10))
+                            valid_source_found.append(host)
+                            logging.info("%s: Route not changed to %s, rtt difference %s < threshold %s",
+                                         host, host_data[0], round(rtt_diff, 3), self.config.get('rtt_threshold', 10))
                             continue
 
+                    valid_source_found.append(host)
                     self.apply_route_config(host, host_data[0])
 
                 checks = 0
                 sums = {}
+
+                for sip in self.config['monitor']:
+                    if sip in valid_source_found:
+                        continue
+                    logging.warning("No valid source found for %s", sip)
+
+                    # fallback routes
+                    if self.config.get('fallback_routes', {}).get(sip):
+                        self.apply_route_config(sip, self.config.get('fallback_routes', {}).get(sip))
+                    else:
+                        logging.warning("No fallback routes configured for %s", sip)
+                        self.clear_route(sip)
 
             await asyncio.sleep(self.config.get('scan_interval', 10))
 
